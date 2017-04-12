5d18586657eb1ff016cc07181dae8a366848eb41
Orelab <aurelien.chirot@idee-lab.fr>
2017-04-12 11:52:59 +0200

    corrigé

--- app.js
+++ app.js
@@ -1,16 +1,104 @@
-	$(document).ready(function(){
-		//alert(bonjour);
-		var createPoint =function(){
-		};
-				//creation du nombre de point l'utillisateur choisira
-			var pointNumber = prompt("Veuillez saisir le nombre de point");	
-					//console.log(pointNumber);	
-			for(var i = 0; i <= pointNumber; i++) {
-				$('<div class="debris"></div>').appendTo("#jeu");
 
-			};
-	});
+
+var nbDebris = 200;
+var puissance = 40;
+
+
+$(document).ready(function(){
 
 
 
+	// Génération de saleté
 
+	for( var i=0 ; i<nbDebris ; i++ ) {
+
+		$('<div/>').addClass('debris').css({
+			top: Math.random() * 490,
+			left: Math.random() * 490
+		}).appendTo('#jeu');
+	}
+
+
+	var actif = false;
+
+
+
+	// Activer l'électro-aimant
+
+	$('#jeu').on('mousedown', function(){
+		actif = true;
+	})
+
+
+	// Désactiver l'électro-aimant
+
+	$('#jeu').on('mouseup', function(){
+		actif = false;
+	})
+
+
+	// Coller la saleté - version 1
+
+	var collerV1 = function( e ){
+
+		if( ! actif ) return;
+
+		var sourisX = e.pageX;
+		var sourisY = e.pageY;
+
+		$('.debris').each(function(id,s) {
+
+			var offset = $(s).offset();
+
+			var debrisX = offset.left;
+			var debrisY = offset.top;
+
+			if( debrisX < sourisX+puissance
+				&& debrisX > sourisX-puissance
+				&& debrisY < sourisY+puissance 
+				&& debrisY > sourisY-puissance ) {
+
+				$(this).offset({ top:sourisY, left:sourisX });
+			}
+		});
+	}
+
+
+	// Coller la saleté - version 2
+
+	var collerV2 = function( e ){
+
+		if( ! actif ) return;
+
+		var sourisX = e.pageX;
+		var sourisY = e.pageY;
+		var cadreOffset = $(this).offset();
+
+		$('.debris').each(function(id,s) {
+
+			var offset = $(s).offset();
+
+			var debrisX = offset.left;
+			var debrisY = offset.top;
+
+			if( debrisX < sourisX +puissance
+				&& debrisX > sourisX-puissance
+				&& debrisY < sourisY+puissance 
+				&& debrisY > sourisY-puissance ) {
+
+				$(this).stop().animate({
+					top: sourisY - cadreOffset.top,
+					left: sourisX - cadreOffset.left
+				}, 100);
+
+			}
+		});
+	}
+
+
+	$('#jeu').on('mousemove', collerV2 );
+
+
+
+
+});
